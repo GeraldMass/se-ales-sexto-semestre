d12a2803999f0c761e49069d775f06feb40b3eeb
Gerald Mass <gmass@uninorte.edu.co>
2022-07-28 20:05:12 -0500

    en esta version se añadio un comentario pendejo

---
+++ primer_lab.py
@@ -0,0 +1,6 @@
+print("hola peter digo python")
+print("hola mama")
+print("hola mama")
+# print("hola mama") control + kc se mantiene el control le das despues a letra k y por ultimo la letra 
+print("hola mama")
+print("hola mama")

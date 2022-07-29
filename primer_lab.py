753f3b850793a563ae87ea6e5c6fa4439622f708
Gerald Mass <gmass@uninorte.edu.co>
2022-07-28 20:10:29 -0500

    esta version tiene mi primera funcion

--- primer_lab.py
+++ primer_lab.py
@@ -4,3 +4,9 @@ print("hola mama")
 # print("hola mama") control + kc se mantiene el control le das despues a letra k y por ultimo la letra 
 print("hola mama")
 print("hola mama")
+
+def mi_primera_funcion()
+    print("esta es mi primera funcion")
+
+
+mi_primera_funcion()
